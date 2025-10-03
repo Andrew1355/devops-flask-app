7263c2d68079d05ca439c92e30a6caa8e9e88c8a
Andrew1355 <53454235+Andrew1355@users.noreply.github.com>
2025-10-03 11:45:16 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def say_hello():
+	return "<p>Welcome, Flask!</p><a href='/about'>About</a><a href='/contact'>Contact</a>"
+
+@app. route("/about")
+def say_about():
+	return "<p>About!</p> <a href='/'>Home</a><br><a href=\"https://www.python.org/\">Pytohn website</a>"
+
+@app. route("/contact")
+def say_contact():
+        return "<p>Contact! C23362336@mytudublin.ie</p> <a href='/'>Home</a>"
